72e39a5ce49f213ed830408eabac89787445d43f
Mohamed Aziz Ammar <108346635+aziz4ammar@users.noreply.github.com>
2023-01-26 08:16:21 +0100

    Final Commit

--- Mohamed Aziz Ammar.py
+++ Mohamed Aziz Ammar.py
@@ -25,21 +25,21 @@ def tri(t,n):
 def afficher(t,n):
     for i in range(n):
         print(t[i],end="|")
-def verifier(t,n):
-    pd=0
-    pf=n-1
+def ver(t,n):
+    h=0
+    m=n-1
     s=0
-    camion=0
-    while pd<pf :
-        if(t[pd]+t[pf])>10:
-            s=t[pd]+t[pf];camion=camion+1
-            print("\nla somme t["+str(pd)+"]"+"+"+"t["+str(pf)+"]"+"="+str(t[pd])+"+"+str(t[pf])+"="+str(s)+">10 donc camion N"+str(camion)+" a un seul conteneur("+str(t[pd])+")tonnes")
-            pd=pd+1
+    e=0
+    while h<m :
+        if(t[h]+t[m])>10:
+            s=t[h]+t[m];e=e+1
+            print("\nla somme t["+str(h)+"]"+"+"+"t["+str(m)+"]"+"="+str(t[h])+"+"+str(t[m])+"="+str(s)+">10 donc camion N"+str(e)+" a un seul conteneur("+str(t[h])+")tonnes")
+            h=h+1
         else:
-            s=t[pd]+t[pf];camion=camion+1
-            print("\nla somme t["+str(pd)+"]"+"+"+"t["+str(pf)+"]"+"="+str(t[pd])+"+"+str(t[pf])+"="+str(s)+"<=10 donc camion N"+str(camion)+" a conteneurs double("+str(t[pd])+","+str(t[pf])+")tonnes")
-            pd=pd+1;pf=pf-1
-    print("\nle nombre de camions a louer est:",camion)
+            s=t[h]+t[m];e=e+1
+            print("\nla somme t["+str(h)+"]"+"+"+"t["+str(m)+"]"+"="+str(t[h])+"+"+str(t[m])+"="+str(s)+"<=10 donc camion N"+str(e)+" a conteneurs double("+str(t[h])+","+str(t[m])+")tonnes")
+            h=h+1;m=m-1
+    print("\nle nombre de camions a louer est:",e)
 
 
 
@@ -71,4 +71,4 @@ t=np.array([float]*n)
 remplir(t,n)
 tri(t,n)
 afficher(t,n)
-verifier(t,n)
+verif(t,n)
